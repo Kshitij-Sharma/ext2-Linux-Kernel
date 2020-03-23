3f9ba6745ce436c82837425b24d7da6cb8625534
Pavan Baloo <pbaloo2@illinois.edu>
2020-03-23 17:23:00 -0500

    exceptions pushed with asm wrappers

---
+++ student-distrib/idt_exception_wrappers.h
@@ -0,0 +1,33 @@
+#ifndef _IDT_EXCEPTION_WRAPPERS_H
+#define _IDT_EXCEPTION_WRAPPERS_H
+#ifndef ASM
+
+
+// #include "types.h"
+#include "i8259.h"
+#include "lib.h"
+#include "idt_handlers.h"
+
+extern void divide_by_zero();
+extern void debug();
+extern void non_maskable_interrupt();
+extern void breakpoint();
+extern void into_detected_overflow();
+extern void out_of_bounds();
+extern void invalid_opcode();
+extern void no_coprocessor();
+extern void double_fault();
+extern void coprocessor_segment_overrun();
+extern void bad_tss();
+extern void segment_not_present();
+extern void stack_fault();
+extern void general_protection_fault();
+extern void page_fault();
+extern void unknown_interrupt();
+extern void coprocessor_fault();
+extern void alignment_check();
+extern void machine_check();
+extern void SIMD_floating_point();
+
+#endif
+#endif
