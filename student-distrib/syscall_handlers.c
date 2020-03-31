727f4c15a8bd6f5d905773b5dfff85dafa1089f4
Pavan Baloo <pbaloo2@illinois.edu>
2020-03-30 19:44:13 -0500

    move sum shit around ykno

--- student-distrib/syscall_handlers.c
+++ student-distrib/syscall_handlers.c
@@ -23,15 +23,6 @@
 int32_t sys_halt (int8_t status){
     return 0; 
 }
-
-/** sys_execute
- *  
- * execute system call 
- * Inputs: command
- * Outputs: 0
- * Side Effects: None
- * NOT YET IMPLEMENTED
- */
 int32_t sys_execute (const int8_t* command){
     return 0;
 }
@@ -139,9 +130,29 @@ int32_t sys_sigreturn (void){
 
 
 /**
- *  READ HELPERS 
+ *  Terminal 
  **/
 
+/** _sys_open_file
+ *  
+ * File helper function for system open
+ * Inputs: same as sys_open
+ * Outputs: 0 @ directory end
+ * Side Effects: 
+ */
+int32_t _sys_open_terminal (const int8_t* filename){
+    return 0;
+}
+/** _sys_close_file
+ *  
+ * File helper function for system close
+ * Inputs: same as sys_close
+ * Outputs: 0 @ directory end
+ * Side Effects: 
+ */
+int32_t _sys_close_terminal(int32_t fd){
+    return 0;
+}
 /** _sys_read_terminal
  *  
  * Terminal helper function for system read
@@ -169,56 +180,6 @@ int32_t _sys_read_terminal (int32_t fd, void* buf, int32_t nbytes){
 
     return nbytes;
 }
-/** _sys_read_rtc
- *  
- * RTC helper function for system read
- * Inputs: file descriptor, buffer (to fill), number of bytes to read
- * Outputs: 0 when an RTC interrupt occurs
- * Side Effects: none
- */
-int32_t _sys_read_rtc (int32_t fd, void* buf, int32_t nbytes){
-    RTC_READ_FLAG = 1;      // sets a global flag
-    while(RTC_READ_FLAG);   // waits for interrupt
-    // printf("read rtc\n");
-    return 0;
-}
-/** _sys_read_file
- *  
- * Terminal helper function for system read
- * Inputs: file descriptor, buffer (to fill), number of bytes to read
- * Outputs: 0 if success (read nbytes), -1 if fail (read < nbytes)
- * Side Effects: moves forward in file
- */
-int32_t _sys_read_file (int32_t fd, void* buf, int32_t nbytes){
-    int32_t data_read;
-    /* condition checks */
-    if(nbytes <= 0 || buf == NULL || fd < 0) return -1;
-    /* does the data read */
-    data_read = read_data(fd, data_bytes_read, buf, nbytes);
-    /* moves forward in file corresponding to how many bytes are read */
-    if(data_read > 0)                       data_bytes_read += data_read;
-    /* checks that we have read ALL bytes requested */
-    if(data_read != nbytes)                 return -1;
-    return data_read;
-}
-/** _sys_read_directory
- *  
- * Reads the directory and puts all the filenames in a buffer
- * Inputs: file descriptor, buffer (to fill), number of bytes to read
- * Outputs: Returns number of bytes read 
- * Side Effects: Buffer holds a filename from the directory every 32 bytes
- */
-int32_t _sys_read_directory (int32_t fd, void* buf, int32_t nbytes){
-    dentry_t curr_dentry;
-    read_dentry_by_index(fd, &curr_dentry);
-    strncpy((int8_t*) ((fd * FILENAME_LEN) + buf), 
-            (int8_t*) curr_dentry.file_name, FILENAME_LEN);
-    return 0;
-}
-
-/**
- *  WRITE HELPERS 
- */
 /** _sys_write_terminal
  *  
  * Terminal helper function for system write
@@ -258,6 +219,31 @@ int32_t _sys_write_terminal (int32_t fd, void* buf, int32_t nbytes){
     return bytes_written;
 }
 
+/**
+ *  RTC  HELPERS 
+ **/
+/** _sys_open_RTC
+ *  
+ * RTC helper function for system open
+ * Inputs: same as sys_open
+ * Outputs: 0 @ directory end
+ * Side Effects: 
+ */
+int32_t _sys_open_rtc (const int8_t* filename){
+    int freq = 2;
+    _sys_write_rtc(NULL, (void *) freq, 0); // sets the RTC frequency to 2Hz
+    return 0;
+}
+/** _sys_close_RTC
+ *  
+ * RTC helper function for system close
+ * Inputs: same as sys_close
+ * Outputs: 0 @ directory end
+ * Side Effects: 
+ */
+int32_t _sys_close_rtc(int32_t fd){
+    return 0;
+}
 /** sys_write_rtc
  *  
  * RTC helper function for system write
@@ -280,8 +266,8 @@ int32_t _sys_write_rtc(int32_t fd, void* buf, int32_t nbytes){
     if (frequency > MAX_INTERRUPT_FREQUENCY){
         frequency = MAX_INTERRUPT_FREQUENCY;
     }
-    rate = (log_base_two(FREQ_CONVERSION_CONST/frequency)/log_base_two(2)) + 1; // this conversion was found on OSDev and it just an equation to convert frequency to rate
-    if (rate <= 2) rate = 3; // 3 is the lowest the rate can be, so if it is under that, set it to 3
+    rate = (log_base_two(FREQ_CONVERSION_CONST/frequency)/log_base_two(2)) + 1;
+    if (rate <= 2) rate = 3;
 
     // MAGIC NUMBER: 0x0F sets rate selector bits in register A
     outb(RTC_STATUS_REGISTER_A, RTC_CMD_PORT);          // set index to register A, disable NMI
@@ -291,42 +277,24 @@ int32_t _sys_write_rtc(int32_t fd, void* buf, int32_t nbytes){
     // MAGIC NUMBER: 0xF0 is used to clear bottom 4 bits before setting rate
     return 0;
 }
-/** sys_write_rtc
+/** _sys_read_rtc
  *  
- * RTC helper function for system write
- * Inputs: same as sys_write
- * Outputs: 0 @ directory end
- * Side Effects: sets RTC rate
+ * RTC helper function for system read
+ * Inputs: file descriptor, buffer (to fill), number of bytes to read
+ * Outputs: 0 when an RTC interrupt occurs
+ * Side Effects: none
  */
-
-int32_t _sys_write_file (int32_t fd, void* buf, int32_t nbytes){
-    return -1;
-}
-/** sys_write_rtc
- *  
- * RTC helper function for system write
- * Inputs: same as sys_write
- * Outputs: 0 @ directory end
- * Side Effects: sets RTC rate
- */
-
-int32_t _sys_write_directory (int32_t fd, void* buf, int32_t nbytes){
-    return -1;
-}
-
-/**
- *  OPEN HELPERS 
- **/
-/** _sys_open_file
- *  
- * File helper function for system open
- * Inputs: same as sys_open
- * Outputs: 0 @ directory end
- * Side Effects: 
- */
-int32_t _sys_open_terminal (const int8_t* filename){
+int32_t _sys_read_rtc (int32_t fd, void* buf, int32_t nbytes){
+    RTC_READ_FLAG = 1;      // sets a global flag
+    while(RTC_READ_FLAG);   // waits for interrupt
+    // printf("read rtc\n");
     return 0;
 }
+
+
+/**
+ *  FILE  HELPERS 
+ **/
 /** _sys_open_file
  *  
  * File helper function for system open
@@ -337,10 +305,10 @@ int32_t _sys_open_terminal (const int8_t* filename){
 int32_t _sys_open_file (const int8_t* filename){ 
     // dentry_t * this_file; // ***************ADD RETURN VAL CHECKING *****************
     // this_file->inode = -1;
-    int8_t file[MAX_NAME_LENGTH];
+    int8_t file[32];
     int i;
     int ret_val;
-    for (i = 0; i < MAX_NAME_LENGTH; i++)
+    for (i = 0; i < 32; i++)
     { 
         if(filename[i] == '\0')        break;
         file[i] = filename[i];
@@ -358,42 +326,6 @@ int32_t _sys_open_file (const int8_t* filename){
     /* returns inode number with associated file */
     return this_file.inode;
 }
-/** _sys_open_directory
- *  
- * Directory helper function for system write
- * Inputs: same as sys_open
- * Outputs: 0 @ directory end
- * Side Effects: 
- */
-int32_t _sys_open_directory (const int8_t* filename){
-    return 0;
-}
-/** _sys_open_RTC
- *  
- * RTC helper function for system open
- * Inputs: same as sys_open
- * Outputs: 0 @ directory end
- * Side Effects: 
- */
-int32_t _sys_open_rtc (const int8_t* filename){
-    int freq = 2;
-    _sys_write_rtc(NULL, (void *) freq, 0); // sets the RTC frequency to 2Hz
-    return 0;
-}
-
-/**
- *  CLOSE HELPERS 
- **/
-/** _sys_close_file
- *  
- * File helper function for system close
- * Inputs: same as sys_close
- * Outputs: 0 @ directory end
- * Side Effects: 
- */
-int32_t _sys_close_terminal(int32_t fd){
-    return 0;
-}
 /** _sys_close_file
  *  
  * File helper function for system close
@@ -404,14 +336,48 @@ int32_t _sys_close_terminal(int32_t fd){
 int32_t _sys_close_file(int32_t fd){
     return 0;
 }
-/** _sys_close_RTC
+/** _sys_read_file
  *  
- * RTC helper function for system close
- * Inputs: same as sys_close
+ * Terminal helper function for system read
+ * Inputs: file descriptor, buffer (to fill), number of bytes to read
+ * Outputs: 0 if success (read nbytes), -1 if fail (read < nbytes)
+ * Side Effects: moves forward in file
+ */
+int32_t _sys_read_file (int32_t fd, void* buf, int32_t nbytes){
+    int32_t data_read;
+    /* condition checks */
+    if(nbytes <= 0 || buf == NULL || fd < 0) return -1;
+    /* does the data read */
+    data_read = read_data(fd, data_bytes_read, buf, nbytes);
+    /* moves forward in file corresponding to how many bytes are read */
+    if(data_read > 0)                       data_bytes_read += data_read;
+    /* checks that we have read ALL bytes requested */
+    if(data_read != nbytes)                 return -1;
+    return data_read;
+}
+/** sys_write_file
+ *  
+ * RTC helper function for system write
+ * Inputs: same as sys_write
+ * Outputs: 0 @ directory end
+ * Side Effects: sets RTC rate
+ */
+
+int32_t _sys_write_file (int32_t fd, void* buf, int32_t nbytes){
+    return -1;
+}
+
+/**
+ *  DIRECTORY  HELPERS 
+ **/
+/** _sys_open_directory
+ *  
+ * Directory helper function for system write
+ * Inputs: same as sys_open
  * Outputs: 0 @ directory end
  * Side Effects: 
  */
-int32_t _sys_close_rtc(int32_t fd){
+int32_t _sys_open_directory (const int8_t* filename){
     return 0;
 }
 /** _sys_close_directory
@@ -424,6 +390,31 @@ int32_t _sys_close_rtc(int32_t fd){
 int32_t _sys_close_directory(int32_t fd){
     return 0;
 }
+/** _sys_read_directory
+ *  
+ * Reads the directory and puts all the filenames in a buffer
+ * Inputs: file descriptor, buffer (to fill), number of bytes to read
+ * Outputs: Returns number of bytes read 
+ * Side Effects: Buffer holds a filename from the directory every 32 bytes
+ */
+int32_t _sys_read_directory (int32_t fd, void* buf, int32_t nbytes){
+    dentry_t curr_dentry;
+    read_dentry_by_index(fd, &curr_dentry);
+    strncpy((int8_t*) ((fd * FILENAME_LEN) + buf), 
+            (int8_t*) curr_dentry.file_name, FILENAME_LEN);
+    return 0;
+}
+/** sys_write_directory
+ *  
+ * RTC helper function for system write
+ * Inputs: same as sys_write
+ * Outputs: 0 @ directory end
+ * Side Effects: sets RTC rate
+ */
+
+int32_t _sys_write_directory (int32_t fd, void* buf, int32_t nbytes){
+    return -1;
+}
 
 
 
@@ -435,6 +426,18 @@ int32_t _sys_close_directory(int32_t fd){
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 int32_t _sys_dummy_read_write(int32_t fd, void* buf, int32_t nbytes){
     return -1;
 }
